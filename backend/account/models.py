e3c9be3adf8ffb62c402e82c0bf7d9c966f1e66b
Kevin Heleodoro <631682@bah.com>
2024-07-18 08:00:33 -0400

    added server and account apps

---
+++ backend/account/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Adding functionality to the user table
+class Account(AbstractUser):
+    pass
---
+++ backend/server/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+
+class Server(models.Model):
+    pass
