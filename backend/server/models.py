fce4b146737718f535c0e38d1b563cff36512a71
Kevin Heleodoro <631682@bah.com>
2024-07-18 10:05:31 -0400

    models for server, channel, and category

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -5,6 +6,35 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Server(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    # 1 to 1 connection to owner
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner")
+    # should use models.PROTECT
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="server_category")
+    description = models.CharField(max_length=250, blank=True, null=True)
+    # 1 to many users
+    # this will generate a separate table to keep track of what members belong to a server
+    member = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
+    topic = models.CharField(max_length=100)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
+
+    def save(self, *args, **kwargs):
+        # enforce name is lowercase
+        self.name = self.name.lower()
+        super(Channel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
